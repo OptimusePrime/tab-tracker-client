ef0ba5cb6e91eceafc8bc860314a665d2e9c54ba
BuildTools <unconfigured@null.spigotmc.org>
2022-06-16 14:38:22 +0200

    added layouts

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'default.vue'
+}
+</script>
+
+<style scoped>
+
+</style>
